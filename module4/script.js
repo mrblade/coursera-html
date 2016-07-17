0304d139c664e5a14e21cf8333b85bf405e41b0e
Harsimran Singh <C:\Users\Harsimran\AppData\Roaming\The Bat!>
2016-07-17 20:20:26 +0530

    Rev

--- module4/script.js
+++ module4/script.js
@@ -37,8 +37,8 @@ WARNING!!! WARNING!!!
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	
 	for (var i=0 ; i < names.length ; i++) {
-		var firstLetter = names[i].charAt(0);
-		if (firstLetter.toLowerCase() == "j") {
+		var firstLetter = names[i].charAt(0).toLowerCase();
+		if (firstLetter == "j") {
      byeSpeaker.speak(names[i]);
   } else {
      helloSpeaker.speak(names[i]);
