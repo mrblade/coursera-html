ece8bdc87799e908228033402bb5f7409d05f937
Harsimran Singh <C:\Users\Harsimran\AppData\Roaming\The Bat!>
2016-09-11 01:08:51 +0530

    update

--- assignment1-solution-module5/js/app.js
+++ assignment1-solution-module5/js/app.js
@@ -8,19 +8,16 @@
 	function LunchCheckController($scope) {
 			
 		$scope.values="";
-		$scope.txtColor="";
-		$scope.borderColor="";
+		
 		$scope.sayMsg = function () {
 								
 		if ($scope.values.trim() === "") {
-			$scope.txtColor="red";
-			$scope.borderColor="red";
+			
 			$scope.msg="Please enter data first";
 		}
 		
 		else {
-		$scope.txtColor="green";		
-		$scope.borderColor="green";
+		
 		var elements =$scope.values.split(',');
 	 	if(elements.length <= 3){
 			$scope.msg="Enjoy!";
